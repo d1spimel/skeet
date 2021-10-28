887de6eb8aa056292d9e8a1d9b8840e0a655813a
d1spimel <73076512+d1spimel@users.noreply.github.com>
2021-10-28 19:51:03 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float y, x, min, max, h;
+	cout << "Введите min: ";
+	cin >> min;
+	cout << "Введите max: ";
+	cin >> max;
+	cout << "Введите h: ";
+	cin >> h;
+	x = min;
+	cout << setw(10) << "F(x): " << setw(10) << "F(y): " << endl;
+	while (x <= max + h / 2)
+	{
+		y = pow(x, 8) + 5 * pow(x, 2) - 5;
+		cout << setw(10) << setprecision(5) << x << setw(10) << setprecision(5) << y << endl;
+		x += h;
+	}
+}
